a150a0615472803ff1fe416fdf3ce1d6a0d2d32e
John Doe <johndoe@email.com>
2024-09-19 00:25:06 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,8 @@
+//Task 1: Create an Inventory Array of Product Objects
+
+const inventory = [
+    { name: 'Diet Coke', price: 4.00, quantity: 25 },
+    { name: 'Protein Bar', price: 3.00, quantity: 18 },
+    { name: 'Protein Shake', price: 4.50, quantity: 15 },
+    { name: 'Salad', price: 7.50, quantity: 10 }
+];
